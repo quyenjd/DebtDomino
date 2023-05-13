aa3006dbab445bc25c80304675fc5903aa2e631b
houssam23 <19270989@students.latrobe.edu.au>
2023-05-13 16:30:12 +1000

    Added registration page and main page for debt tracking

--- app/src/main/java/com/example/debtdomino/ProgressTrackingActivity.java
+++ app/src/main/java/com/example/debtdomino/ProgressTrackingActivity.java
@@ -1,4 +1,23 @@
 package com.example.debtdomino;
 
-public class ProgressTrackingActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProgressTrackingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_tracking);
+
+        if (getSupportActionBar() != null) { // null check
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true); // show back button
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed(); // when back button is pressed, go back to the previous activity
+        return true;
+    }
 }
--- app/src/main/java/com/example/debtdomino/RegistrationActivity.java
+++ app/src/main/java/com/example/debtdomino/RegistrationActivity.java
@@ -1,9 +1,15 @@
 package com.example.debtdomino;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,16 @@ public class RegistrationActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) { // null check
             getSupportActionBar().setDisplayHomeAsUpEnabled(true); // show back button
         }
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistrationActivity.this, ProgressTrackingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
