038875911333636ffed4fb79750091655c4adceb
houssam23 <19270989@students.latrobe.edu.au>
2023-05-21 22:47:12 +1000

    Removed unecessary methods

--- app/src/main/java/com/example/debtdomino/NotificationsUtils.java
+++ app/src/main/java/com/example/debtdomino/NotificationsUtils.java
@@ -26,17 +26,4 @@ public class NotificationsUtils extends FirebaseMessagingService {
             //Handle the notification message here
         }
     }
-
-    @Override
-    public void onNewToken(String token) {
-        Log.d(TAG, "Refreshed token: " + token);
-        //If you want to send messages to this application instance or
-        //manage this apps subscriptions on the server side, send the
-        //Instance ID token to your app server.
-        sendRegistrationToServer(token);
-    }
-
-    private void sendRegistrationToServer(String token) {
-        // TODO: Implement this method to send token to your app server.
-    }
 }
